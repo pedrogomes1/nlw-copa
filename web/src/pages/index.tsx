03cb3b4226d752b22d88dbd8e3f43ad68a78078d
Pedro Gomes <pedro-gomes@ibm.com>
2022-11-13 14:02:24 -0300

    chore: finish all styles web app with tailwind

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,51 +6,66 @@ import userAvatarExampleImg from "../assets/users-avatar-example.png";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-[1124px] mx-auto h-screen grid gap-28 grid-cols-2 items-center">
       <main>
         <Image src={logoImg} alt="Logo da aplicação NLW Copa" />
 
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos</h1>
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos
+        </h1>
 
-        <div>
+        <div className="mt-10 flex items-center gap-2">
           <Image
             src={userAvatarExampleImg}
             alt="Avatares de pessoas que estão utilizando a aplicação"
           />
-          <strong>
-            <span>+12.592</span> pessoas já estão usando
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão
+            usando
           </strong>
+        </div>
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            type="text"
+            required
+            placeholder="Qual nome do seu bolão?"
+          />
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
+        </form>
 
-          <form>
-            <input type="text" required placeholder="Qual nome do seu bolão?" />
-            <button type="submit">Criar meu bolão</button>
-          </form>
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas 🚀
+        </p>
 
-          <p>
-            Após criar seu bolão, você receberá um código único que poderá usar
-            para convidar outras pessoas 🚀
-          </p>
-
-          <div>
-            <div>
-              <Image
-                src={iconCheck}
-                alt="Ícone redondo da cor verde com símbolo de check branco"
-              />
-              <div>
-                <span>+2.034</span>
-                <span>Bolões criados</span>
-              </div>
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
+            <Image
+              src={iconCheck}
+              alt="Ícone redondo da cor verde com símbolo de check branco"
+            />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Bolões criados</span>
             </div>
-            <div>
-              <Image
-                src={iconCheck}
-                alt="Ícone redondo da cor verde com símbolo de check branco"
-              />
-              <div>
-                <span>+2.034</span>
-                <span>Bolões criados</span>
-              </div>
+          </div>
+
+          <div className="w-px h-14 bg-gray-600" />
+
+          <div className="flex items-center gap-6">
+            <Image
+              src={iconCheck}
+              alt="Ícone redondo da cor verde com símbolo de check branco"
+            />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Palpites enviados</span>
             </div>
           </div>
         </div>
