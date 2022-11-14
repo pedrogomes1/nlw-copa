776af3a75cc908561608eebab5f19f40cdb0efe2
Pedro Gomes <pedro-gomes@ibm.com>
2022-11-14 10:00:48 -0300

    chore: create new route /me to get user info and modify post user to create new user when not is registered

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get('/me', {onRequest: [authenticate]},
+   async (request) => {
+    return { user: request.user }
+  })
+
   fastify.post('/users', async (request,reply) => {
     const createUserBody = z.object({
       access_token: z.string()
@@ -28,7 +34,31 @@ export async function authRoutes(fastify: FastifyInstance) {
 
     const userInfo = userInfoSchema.parse(userData);
 
+    let user = await prisma.user.findUnique({
+      where: {
+        googleId: userInfo.id
+      }
+    })
+  
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.id,
+          name: userInfo.name,
+          email: userInfo.email,
+          avatarUrl: userInfo.picture
+        }
+      })
+    }
 
-    return { userInfo }
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl
+    }, {
+      sub: user.id,
+      expiresIn: '7 days'
+    })
+
+    return { token }
   })
 }
