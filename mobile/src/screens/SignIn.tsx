4f766b27587d9c6393049ca39b35e818563f6da6
Pedro Gomes <pedro-gomes@ibm.com>
2022-11-14 11:59:20 -0300

    feat: finish SignIn screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,25 @@
-import { NativeBaseProvider, Center, Text, StatusBar } from "native-base";
+import { Center, Icon, Text } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
+
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>
-        Hello world
+    <Center flex={1} bgColor="gray.900" p={8}>
+      <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        type="SECONDARY"
+        mt={12}
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
+        de sua conta.
       </Text>
-      <StatusBar barStyle="dark-content" />
     </Center>
   );
 }
